8bd2a9a9b7331e434337cc2d9971e28c553a94a7
ArhipKokocan <hts1345hjyuhip228888@gmail.com>
2024-06-02 10:53:31 +0300

    Add main entrypoint

--- core/main.py
+++ core/main.py
@@ -1,5 +1,14 @@
 from fastapi import FastAPI
 
+from core.api.v1.handlers import router as transcription_router
 
 
-app = FastAPI()
+def create_app() -> FastAPI:
+    app = FastAPI(
+        title="Transciber Microservice",
+        docs_url="/api/docs",
+        description="This microservice provides endpoints for transcribing audio and video files using the Whisper API."
+    )
+    app.include_router(transcription_router)
+    
+    return app
